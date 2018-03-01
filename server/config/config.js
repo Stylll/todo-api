918ff3a52d3889ed39a3e619378a9e7f2949f252
Styll <stephen.aribaba@yahoo.com>
2018-03-01 14:02:49 +0100

    testing process setup

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,9 @@ const config = {
   },
   test: {
     use_env_variable: 'TEST_DATABASE_URL',
+    database: 'test_todo_dev',
+    username: 'postgres',
+    password: null,
   },
   production: {
     use_env_variable: 'PRODUCTION_DATABASE_URL',
