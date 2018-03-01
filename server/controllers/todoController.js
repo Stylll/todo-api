24215057efcf690e2cdc483170598b0873cb8b28
Styll <stephen.aribaba@yahoo.com>
2018-03-01 15:48:18 +0100

    lf update

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -46,15 +46,15 @@ module.exports.findById = (req, res) => Todo.findById(req.params.id, {
 module.exports.update = (req, res) => Todo.findById(req.params.id, {
   include: ['todoItems'],
 }).then((todo) => {
-    if (!todo) {
-      return res.status(404).send({ message: 'Todo not found' });
-    }
-    return todo.update({
-      title: req.body.title || todo.title,
-    })
-      .then(todoSingle => res.status(200).send(todoSingle))
-      .catch(error => res.status(400).send(error));
+  if (!todo) {
+    return res.status(404).send({ message: 'Todo not found' });
+  }
+  return todo.update({
+    title: req.body.title || todo.title,
   })
+    .then(todoSingle => res.status(200).send(todoSingle))
+    .catch(error => res.status(400).send(error));
+})
   .catch(error => res.status(400).send(error));
 
 /**
@@ -65,12 +65,12 @@ module.exports.update = (req, res) => Todo.findById(req.params.id, {
 module.exports.delete = (req, res) => Todo.findById(req.params.id, {
   include: ['todoItems'],
 }).then((todo) => {
-    if (!todo) {
-      return res.status(404).send({ message: 'Todo not found' });
-    }
-    return todo.destroy()
-      .then(todoSingle => res.status(204).send())
-      .catch(error => res.status(400).send(error));
-  })
+  if (!todo) {
+    return res.status(404).send({ message: 'Todo not found' });
+  }
+  return todo.destroy()
+    .then(todoSingle => res.status(204).send())
+    .catch(error => res.status(400).send(error));
+})
   .catch(error => res.status(400).send(error));
 
