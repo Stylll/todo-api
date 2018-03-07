9ae127aae7d17a06bf53287b80726b37aca31c4b
Styll <stephen.aribaba@yahoo.com>
2018-03-07 17:42:45 +0100

    fix for travis ci not exiting mocha

---
+++ server/test/controllers/user.test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import app from '../../../server';
+import { insertBulkTodo, insertBulkTodoItems, truncateTables } from '../helpers/seedGenerator';
+
+/* eslint-disable no-undef */
+describe('USER CONTROLLER', () => {
+  describe('POST: Signup User - /api/user/signup', () => {
+
+  });
+
+  describe('POST: Login User - /api/user/login', () => {
+
+  });
+});
