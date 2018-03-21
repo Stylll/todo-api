39c58679ca80bf301f08cda8ea976831600ac91c
Styll <stephen.aribaba@yahoo.com>
2018-03-22 00:19:28 +0100

    heroku production build port fix

--- tools/distServer.js
+++ tools/distServer.js
@@ -8,7 +8,7 @@ import routes from '../server/routes/route';
 
 /* eslint-disable no-console */
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(compression());
